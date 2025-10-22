a94f6dd0be2add612c6346a9ae4e6388d5356d3b
anvilm <211anvil112@gmail.com>
2025-10-22 15:42:24 +0300

    fix(config): add logger config

--- app/Config/LoggerConfig/LoggerConfig.php
+++ app/Config/LoggerConfig/LoggerConfig.php
@@ -4,7 +4,20 @@ declare(strict_types=1);
 
 namespace Application\Config\LoggerConfig;
 
+use Application\Config\ApplicationConfig\ApplicationConfig;
+use Application\Config\ApplicationConfig\ApplicationEnvironmentEnum;
+use Monolog\Level;
+
 final readonly class LoggerConfig
 {
+    public static function path(): string
+    {
+        return ApplicationConfig::baseDir() . '/logs/app.log';
+    }
 
+
+    public static function level(): Level
+    {
+        return ApplicationConfig::appEnv() === ApplicationEnvironmentEnum::Production ? Level::Info : Level::Debug;
+    }
 }
