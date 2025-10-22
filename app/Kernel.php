4ad584836587f9831b9feca0b4d0366819cb0298
anvilm <211anvil112@gmail.com>
2025-10-22 15:45:15 +0300

    feat(app): add kernel

--- app/Kernel.php
+++ app/Kernel.php
@@ -4,7 +4,25 @@ declare(strict_types=1);
 
 namespace Application;
 
+use Application\Bootstrappers\ApplicationBootstrapper;
+use Application\Bootstrappers\ContainerBootstrapper;
+use Application\Bootstrappers\EndpointsBootstrapper;
+use Application\Bootstrappers\ProvidersBootstrapper;
+use Slim\App;
+
 final readonly class Kernel
 {
 
+    public static function createApp(): App
+    {
+        $app = ApplicationBootstrapper::createApp(
+            ContainerBootstrapper::createContainer(
+                ProvidersBootstrapper::getProviders()
+            )
+        );
+
+        EndpointsBootstrapper::registerEndpoints($app);
+
+        return $app;
+    }
 }
