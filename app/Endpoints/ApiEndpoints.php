c68fdda87f12080d576946057103c3bc5bef9681
anvilm <211anvil112@gmail.com>
2025-10-22 15:43:46 +0300

    feat(endpoints): add endpoints

--- app/Endpoints/ApiEndpoints.php
+++ app/Endpoints/ApiEndpoints.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 namespace Application\Endpoints;
 
-final readonly class ApiEndpoints
-{
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\App;
 
+final readonly class ApiEndpoints implements EndpointInterface
+{
+    public static function register(App $app): void
+    {
+        $app->get('/', function (RequestInterface $request, ResponseInterface $response) {
+            $response->getBody()->write('Example response');
+            return $response;
+        });
+    }
 }
--- app/Endpoints/EndpointInterface.php
+++ app/Endpoints/EndpointInterface.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Application\Endpoints;
 
+use Slim\App;
+
 interface EndpointInterface
 {
-
+    public static function register(App $app): void;
 }
