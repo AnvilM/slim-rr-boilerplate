af268c82bd1b2d1d14578e7c9d14a07ec4541f7e
anvilm <211anvil112@gmail.com>
2025-10-23 17:32:08 +0300

    feat(commands): add create cli app method

--- app/Kernel.php
+++ app/Kernel.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Application;
 
 use Application\Bootstrappers\ApplicationBootstrapper;
+use Application\Bootstrappers\CommandsBootstrapper;
 use Application\Bootstrappers\ContainerBootstrapper;
 use Application\Bootstrappers\EndpointsBootstrapper;
 use Application\Bootstrappers\ProvidersBootstrapper;
 use Slim\App;
+use Symfony\Component\Console\Application as CliApp;
 
 final readonly class Kernel
 {
@@ -22,6 +24,18 @@ final readonly class Kernel
         );
 
         EndpointsBootstrapper::registerEndpoints($app);
+
+        return $app;
+    }
+
+    public static function createCliApp(): CliApp
+    {
+        $app = new CliApp();
+
+        CommandsBootstrapper::registerCommands($app,
+            ContainerBootstrapper::createContainer(
+                ProvidersBootstrapper::getProviders()
+            ));
 
         return $app;
     }
