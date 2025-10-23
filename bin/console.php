a97e73738e5d828fb1cb3a2abb5a87f310b74126
anvilm <211anvil112@gmail.com>
2025-10-23 17:32:39 +0300

    feat(commands): add cli entrypoint

--- bin/console.php
+++ bin/console.php
@@ -1,3 +1,13 @@
 <?php
 
 declare(strict_types=1);
+
+use Application\Kernel;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+Kernel::createCliApp()->run(); 
