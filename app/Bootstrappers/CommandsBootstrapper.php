f785ffd401e79136c6b0c7898cf8193d9df37282
anvilm <211anvil112@gmail.com>
2025-10-23 17:31:31 +0300

    feat(commands): add commands bootstrapper

--- app/Bootstrappers/CommandsBootstrapper.php
+++ app/Bootstrappers/CommandsBootstrapper.php
@@ -4,7 +4,27 @@ declare(strict_types=1);
 
 namespace Application\Bootstrappers;
 
-final readonly class CommandsBootstrapper
-{
+use Application\Commands\MakeMigrationCommand\MigrationGenerateCommand;
+use Application\Commands\MigrateCommand\MigrationUpCommand;
+use DI\Container;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Application as CliApp;
 
+
+final class CommandsBootstrapper
+{
+    private static array $commands = [
+        MigrationGenerateCommand::class,
+        MigrationUpCommand::class,
+    ];
+
+
+    public static function registerCommands(CliApp $app, ContainerInterface $container): void
+    {
+        /** @var Container $container */
+
+        foreach (self::$commands as $command) {
+            $app->add($container->make($command));
+        }
+    }
 }
