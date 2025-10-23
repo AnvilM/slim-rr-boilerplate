c3407ffd04683d2443be1a2346b5c47b10604a39
anvilm <211anvil112@gmail.com>
2025-10-23 22:22:22 +0300

    feat: add roadrunner

--- index.php
+++ index.php
@@ -3,8 +3,33 @@
 declare(strict_types=1);
 
 use Application\Kernel;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7\Response;
+use Spiral\RoadRunner\Http\PSR7Worker;
+use Spiral\RoadRunner\Worker;
 
 require __DIR__ . '/vendor/autoload.php';
-echo '<pre>';
 
-Kernel::createApp()->run(); 
+$app = Kernel::createApp();
+
+
+$worker = Worker::create();
+
+$factory = new Psr17Factory();
+
+$psr7 = new PSR7Worker($worker, $factory, $factory, $factory);
+
+while (true) {
+    try {
+        $request = $psr7->waitRequest();
+    } catch (Throwable $e) {
+        $psr7->respond(new Response(400));
+        continue;
+    }
+
+    try {
+        $psr7->respond($app->handle($request));
+    } catch (Throwable $e) {
+        $worker->error((string)$e);
+    }
+}
