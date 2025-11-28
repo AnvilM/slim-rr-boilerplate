d36f3afe882aa20f45dc3e3b611d206b03846a81
anvilm <211anvil112@gmail.com>
2025-11-28 22:32:52 +0300

    fix(cli): remove throw anotations

--- app/Bootstrappers/CommandsBootstrapper.php
+++ app/Bootstrappers/CommandsBootstrapper.php
@@ -6,9 +6,7 @@ namespace Application\Bootstrappers;
 
 use Application\Commands\MakeMigrationCommand\MigrationGenerateCommand;
 use Application\Commands\MigrateCommand\MigrationUpCommand;
-use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
-use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Component\Console\Application as CliApp;
 use Symfony\Component\Console\Command\Command;
 
@@ -21,19 +19,15 @@ final class CommandsBootstrapper
         MigrationUpCommand::class,
     ];
 
-
-    /**
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     */
+    
     public static function registerCommands(CliApp $app, ContainerInterface $container): void
     {
 
         foreach (self::$commands as $commandClass) {
             /** @var Command $command */
             $command = $container->get($commandClass);
-            
-            $app->add($command);
+
+            $app->addCommand($command);
         }
     }
 }
