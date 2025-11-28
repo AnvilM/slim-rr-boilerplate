15642fa9b8d22b3afcab861e138f09a28bbe61e3
anvilm <211anvil112@gmail.com>
2025-11-28 22:02:15 +0300

    fix(tests): add app create to test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use Application\Kernel;
 use PHPUnit\Framework\TestCase as BaseTestCase;
+use Slim\App;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected App $app;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->app = Kernel::createApp();
+    }
+
 }
