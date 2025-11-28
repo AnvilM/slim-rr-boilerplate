9f2b1e6923e914317fb1123806b8f2041323a9d3
anvilm <211anvil112@gmail.com>
2025-11-28 22:02:40 +0300

    fix(tests): edit example feature test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,5 +1,7 @@
 <?php
 
-test('example', function () {
-    $this
+use Nyholm\Psr7\ServerRequest;
+
+it('example', function () {
+    expect($this->app->handle(new ServerRequest('GET', '/'))->getStatusCode())->toBe(200);
 });
